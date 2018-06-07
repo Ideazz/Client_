54e59fd45becf2eee9697ac5fddf2315df96510a
dhrubesh <dhrubesh97@gmail.com>
2018-06-07 23:55:33 +0530

    basic prototype of issue_4

--- app/components/Home/index.js
+++ app/components/Home/index.js
@@ -1,11 +1,31 @@
-import React, { } from 'react';
+import React, { Component } from 'react';
+import { slide as Menu } from 'react-burger-menu';
 
-const Home = () => (
-  <div>
-    <h2>
-      Home Page!
-    </h2>
-  </div>
-);
+class Home extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      show: false,
+    };
+    this.handleMenu = this.handleMenu.bind(this);
+  }
+
+  handleMenu() {
+    this.setState({ show: !this.state.show });
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleMenu}>This is</button>
+        <Menu isOpen={this.state.show}>
+          <a id="home" className="menu-item" href="/">Home</a>
+          <a id="about" className="menu-item" href="/about">About</a>
+          <a id="contact" className="menu-item" href="/contact">Contact</a>
+        </Menu>
+      </div>
+    );
+  }
+}
 
 export default Home;
